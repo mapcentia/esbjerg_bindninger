df37f5bf6ce6411467ab8dc2f7455f65888623c9
Martin Høgh <mh@mapcentia.com>
2022-01-19 15:30:42 +0100

    Init

---
+++ scripts/update_all.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+
+use app\extensions\esbjerg_bindninger\models\Search;
+use app\extensions\esbjerg_bindninger\api\Search as ApiSearch;
+use \app\models\Database;
+
+header("Content-type: text/plain");
+include_once("../../../conf/App.php");
+new \app\conf\App();
+Database::setDb("esbjerg");
+$conn = new \app\inc\Model();
+$sql = "SELECT * FROM _cowiplan.kommuneplanramme_attr";
+$result = $conn->execQuery($sql);
+echo $conn->PDOerror[0];
+$count = 0;
+$search = new Search();
+
+while ($row = $conn->fetchRow($result)) {
+    $res = $search->go($row["planid"]);
+//    print_r($res);
+
+}
